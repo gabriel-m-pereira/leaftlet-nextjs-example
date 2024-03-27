94ebf2d7f40020db411c8fa2a10a4da159e23ddd
Gabriel <gabrielmoreira@protonmail.com>
2024-03-27 14:57:22 -0300

    create page with leaflet map with 3 markers

---
+++ src/app/components/Map.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+const Map = () => {
+  return (
+    <MapContainer
+      center={[-23.565987, -46.634771]}
+      zoom={14}
+      className="flex-1"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[-23.56593, -46.6348]}>
+        <Popup>
+          Jacaranda Studio A <br /> Jacaranda Studio B <br /> Jacaranda Studio C
+        </Popup>
+      </Marker>
+      <Marker position={[-23.5802, -46.66489]}>
+        <Popup>Jardim Paulista House</Popup>
+      </Marker>
+      <Marker position={[-23.55798, -46.68838]}>
+        <Popup>ClapMe HUB &gt;:3</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
